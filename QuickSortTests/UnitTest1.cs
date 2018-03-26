d3c39e5c5b11b255b56a5464c37e11272de9eaa7
AnnaUshenina <36697662+AnnaUshenina@users.noreply.github.com>
2018-03-26 15:06:36 +0500

    Добавьте файлы проекта.

---
+++ QuickSortTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Быстрая_сортировка;
+
+namespace QuickSortTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ThreeElements()
+        {
+            var array = new[] { 3, 1, 2 };
+            
+            Sort.QuickSort(array, 0, array.Length-1);
+            Assert.AreEqual(1, array[0]);
+            Assert.AreEqual(2, array[1]);
+            Assert.AreEqual(3, array[2]);            
+        }
+
+        [TestMethod]
+        public void HundredElements()
+        {
+            var array = new int [100];
+            for (int i=0;i<100; i++)
+                array[i] = 2;
+            Sort.QuickSort(array, 0, array.Length - 1);
+            for (int i = 0; i < 100; i++)
+                Assert.AreEqual(2, array[i]);
+        }
+
+        [TestMethod]
+        public void ThousandElements()
+        {
+            var array = new int[1000];
+            var rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+                array[i] = rnd.Next();
+            Sort.QuickSort(array, 0, array.Length - 1);
+            for (int i = 100; i < 1000; i=i+100)
+                Assert.IsTrue(array[i]<array[i+1]);
+        }
+
+        [TestMethod]
+        public void NonElements()
+        {
+            var array = new int[0];
+            Sort.QuickSort(array, 0, array.Length - 1);
+            foreach(int e in array)
+                Assert.IsNull(e);
+        }
+
+        [TestMethod]
+        public void ReallyBigArray()
+        {
+            var array = new int[1500000000];
+            var rnd = new Random();
+            for (int i = 0; i < 1500000000; i++)
+                array[i] = rnd.Next();
+            Sort.QuickSort(array, 0, array.Length - 1);
+            for (int i = 0; i < 1500000000; i++)
+                Assert.IsTrue(array[i] < array[i + 1]);
+        }
+    }
+}
---
+++ Быстрая сортировка/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Быстрая_сортировка
+{
+    public class Sort
+    {
+        
+        public static void QuickSort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                var p = Partition(array, start, end);
+                QuickSort(array, start, p - 1);
+                QuickSort(array, p, end);
+            }
+        }
+
+        private static int Partition(int[] array, int start, int end)
+        {
+            var pivot = array[start];
+            var left = start;
+            var right = end;
+            var lenght = array.Length;
+            while (left <= right)
+            {
+                while (array[left] < pivot && left < lenght - 1)
+                    left++;
+                while (array[right] > pivot && right > 0)
+                    right--;
+                if (left <= right)
+                {
+                    var container = array[left];
+                    array[left] = array[right];
+                    array[right] = container;
+                    left++;
+                    right--;
+                }
+            }
+            return left;
+        }
+    }
+}
